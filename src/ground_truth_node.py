94ee3e599d1e3e87a975a7e9302884e4f92d0d44
Kaktusowy500 <k.lewand500@gmail.com>
2022-11-20 12:33:38 +0100

    Add ground truth node to roslaunch

--- src/ground_truth_node.py
+++ src/ground_truth_node.py
@@ -3,11 +3,12 @@ import rospy
 import numpy as np
 from gazebo_msgs.msg import ModelStates
 
+
 class GroundTruthNode:
-    UAV_NAME_PREFIX = "iris"
 
     def __init__(self):
         rospy.init_node('ground_truth_node', anonymous=True)
+        self.uav_name_prefix = rospy.get_param('~uav_name_prefix')
         self.sub_states = rospy.Subscriber(
             "/gazebo/model_states", ModelStates, self.states_callback)
         self.pub_ground_truth = rospy.Publisher(
@@ -18,7 +19,7 @@ class GroundTruthNode:
         names = data.name
         poses = data.pose
         ground_truth_compressed = np.array([[n, p] for n, p in zip(
-            names, poses) if n.startswith(self.UAV_NAME_PREFIX)])
+            names, poses) if n.startswith(self.uav_name_prefix)])
         self.publish_groud_truth(ground_truth_compressed)
 
     def publish_groud_truth(self, ground_truth_array):
@@ -26,6 +27,7 @@ class GroundTruthNode:
         msg.name = ground_truth_array[:, 0]
         msg.pose = ground_truth_array[:, 1]
         self.pub_ground_truth.publish(msg)
+
 
 if __name__ == '__main__':
     ground_truth_node = GroundTruthNode()
