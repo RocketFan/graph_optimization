02cbf22dda29f9d235f444aa551c7b0dcf2c0781
RocketFan <kubal123.tomczak@gmail.com>
2022-11-26 16:00:30 +0000

    Visualizing uavs path

--- src/visualization_node.py
+++ src/visualization_node.py
@@ -1,26 +1,87 @@
 import rospy
 import rostopic
+import random
 
+from geometry_msgs.msg import Point, PoseStamped
 from nav_msgs.msg import Path
+from std_msgs.msg import ColorRGBA
+from visualization_msgs.msg import Marker, MarkerArray
+
+def extract_id(name: str):
+    id = name.replace('uav', '')
+    return int(id)
 
 class PathMarker:
-    def __init__(self):
+    def __init__(self, id: int, color: ColorRGBA):
         self.path = None
+        self.id = id
+        self.color = color
 
     def set_path(self, path: Path):
         self.path = path
+
+    def get_msg(self) -> MarkerArray:
+        markers = MarkerArray()
+
+        if self.path:
+            points_marker = self.create_marker(self.id, Marker.POINTS)
+            lines_marker = self.create_marker(self.id * 1000, Marker.LINE_STRIP)
+
+            for pose in self.path.poses:
+                point: Point = pose.pose.position
+                self.update_points_marker(points_marker, point)
+                self.update_lines_marker(lines_marker, point)
+
+            markers.markers.append(points_marker)
+            markers.markers.append(lines_marker)
+
+        return markers
+
+    def create_marker(self, id, type):
+        marker = Marker()
+        marker.color = self.color
+        marker.id = id
+        marker.type = type
+        marker.header.frame_id = self.path.header.frame_id
+
+        return marker
+
+    def update_points_marker(self, marker: Marker, point: Point):
+        marker.points.append(point)
+        marker.scale.x = 0.2
+        marker.scale.y = 0.2
+
+    def update_lines_marker(self, marker: Marker, point: Point):
+        marker.points.append(point)
+        marker.scale.x = 0.1
 
 class UAVVisualization:
     def __init__(self, name):
         print('Created: ', name)
         self.name = name
-        self.path_marker = PathMarker()
+        self.id = extract_id(name)
+        self.color = self.draw_color()
+        self.path_marker = PathMarker(self.id, self.color)
 
         self.path_sub = rospy.Subscriber(f'/{name}/path', Path, self.path_callback)
+        self.path_pub = rospy.Publisher(f'/{name}/visualization/path1', MarkerArray, queue_size=10)
+
+    def draw_color(self) -> ColorRGBA:
+        color = ColorRGBA()
+        color.r = random.random()
+        color.g = random.random()
+        color.b = random.random()
+        color.a = 1
+
+        return color
 
     def path_callback(self, path: Path):
         print(self.name)
         self.path_marker.set_path(path)
+
+    def publish(self):
+        msg = self.path_marker.get_msg()
+        self.path_pub.publish(msg)
 
 class VisualizationNode:
     def __init__(self):
@@ -31,7 +92,12 @@ class VisualizationNode:
 
     def run(self):
         while not rospy.is_shutdown():
+            self.visualize()
             self.rate.sleep()
+
+    def visualize(self):
+        for uav in self.uavs:
+            uav.publish()
 
     def update(self):
         new_uav_names = self.update_uav_names()
